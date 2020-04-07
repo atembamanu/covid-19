60e749c6a6770437e2f51f2ce97528bc056f0fbd
Emmanuel Atemba <atembaemm07@gmail.com>
2020-04-08 02:42:55 +0300

    fix lint issues

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,17 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    const period = (data.timeToElapse)/3;
-    return {
-        data: input,
-        impact:{
-            currentlyInfected: data.reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * (Math.pow(2, period))
-        },
-        severeImpact:{
-            currentlyInfected: data.reportedCases * 50,
-            infectionsByRequestedTime: currentlyInfected * (Math.pow(2, period))
-        }
+const input = data;
+const period = (data.timeToElapse)/3;
+return {
+    data: input,
+    impact:{
+        currentlyInfected: data.reportedCases * 10,
+        infectionsByRequestedTime: currentlyInfected * (2**period)
+    },
+    severeImpact:{
+        currentlyInfected: data.reportedCases * 50,
+        infectionsByRequestedTime: currentlyInfected * (2**period)
     }
+};
 };
 
 export default covid19ImpactEstimator;
