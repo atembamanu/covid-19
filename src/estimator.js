173f42d84f6e90f51e29ec1fee04c7a0ba3abf15
Emmanuel Atemba <atembaemm07@gmail.com>
2020-04-08 02:54:41 +0300

    eslint

--- src/estimator.js
+++ src/estimator.js
@@ -2,17 +2,17 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const period = (data.timeToElapse) / 3;
   return {
-      data: input,
+    data: input,
 
-      impact:{
-        currentlyInfected: data.reportedCases * 10,
-        infectionsByRequestedTime: impact.currentlyInfected * (2 ** period)
-      },
+    impact:{
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: impact.currentlyInfected * (2 ** period)
+    },
 
-      severeImpact:{
-        currentlyInfected: data.reportedCases * 50,
-        infectionsByRequestedTime: severeImpact.currentlyInfected * (2 ** period)
-      }
+    severeImpact:{
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: severeImpact.currentlyInfected * (2 ** period)
+    }
   };
 };
 
