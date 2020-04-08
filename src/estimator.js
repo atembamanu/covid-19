57b1f3204c34d977c067b1304dc40d2dac6f78a6
Emmanuel Atemba <atembaemm07@gmail.com>
2020-04-08 03:52:56 +0300

    fix undefined variables

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,7 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  const period = (data.timeToElapse) / 3;
   const iCurrentlyInfected = data.reportedCases * 10;
   const sCurrentlyInfected = data.reportedCases * 50;
-  const iInfectionsByRequestedTime = iCurrentlyInfected * (2 ** period);
-  const sInfectionsByRequestedTime = sCurrentlyInfected * (2 ** period);
   return {
     data: input,
 
@@ -13,7 +10,7 @@ const covid19ImpactEstimator = (data) => {
       infectionsByRequestedTime: {
         inDays: 20,
         inWeeks: 120,
-        inMonths:130
+        inMonths: 130
       }
     },
 
@@ -22,7 +19,7 @@ const covid19ImpactEstimator = (data) => {
       infectionsByRequestedTime: {
         inDays: 20,
         inWeeks: 1120,
-        inMonths:11130
+        inMonths: 11130
       }
     }
   };
