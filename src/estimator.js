e139b8042158fd1a1e9968e86e330e1081ad285a
Emmanuel Atemba <atembaemm07@gmail.com>
2020-04-08 04:14:18 +0300

    fix eslint used function before definition

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,5 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  const period = timePeriods(data.periodType);
-  const iCurrentlyInfected = data.reportedCases * 10;
-  const sCurrentlyInfected = data.reportedCases * 50;
-  const iInfectionsByRequestedTime = iCurrentlyInfected * (2 ** period);
-  const sInfectionsByRequestedTime = sCurrentlyInfected * (2 ** period);
   function timePeriods(time){
       if (time == "days") {
         return Math.trunc(time / 3);
@@ -14,6 +9,11 @@ const covid19ImpactEstimator = (data) => {
         return Math.trunc(time * 30 / 3);
       }
   }
+  const period = timePeriods(data.periodType);
+  const iCurrentlyInfected = data.reportedCases * 10;
+  const sCurrentlyInfected = data.reportedCases * 50;
+  const iInfectionsByRequestedTime = iCurrentlyInfected * (2 ** period);
+  const sInfectionsByRequestedTime = sCurrentlyInfected * (2 ** period);
   return {
     data: input,
 
