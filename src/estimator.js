5c44fa4a6d41ae4acb6543c70e64048767f2ad2d
Emmanuel Atemba <atembaemm07@gmail.com>
2020-04-08 03:26:38 +0300

    period calc

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,10 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  const period = ~~((data.timeToElapse) / 3);
+  const period = (data.timeToElapse) / 3;
   const iCurrentlyInfected = data.reportedCases * 10;
   const sCurrentlyInfected = data.reportedCases * 50;
-  const iInfectionsByRequestedTime = iCurrentlyInfected * (2 ** period);
-  const sInfectionsByRequestedTime = sCurrentlyInfected * (2 ** period);
+  const iInfectionsByRequestedTime = iCurrentlyInfected * (2 ** ~~period);
+  const sInfectionsByRequestedTime = sCurrentlyInfected * (2 ** ~~period);
   return {
     data: input,
 
