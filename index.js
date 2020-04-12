3885b90e3dcfb25c151c7de6775db32943f551fe
Emmanuel Atemba <atembaemm07@gmail.com>
2020-04-13 01:15:05 +0300

    change the url data

--- index.js
+++ index.js
@@ -8,28 +8,25 @@ const fs = require('fs');
 //middleware
 server.use(restify.plugins.bodyParser());
 
-server.use(morgan( (tokens, req, res) => {
-    const url = tokens.url(req, res);
-    const trimmedUrl = url.slice(8);
-    const validUrls = ["on-covid-19", "on-covid-19/json", "on-covid-19/xml"];
-    if (validUrls.includes(trimmedUrl)) {
-        const logdata = [
-            req.time(),
-            trimmedUrl,
-            'done in ' + tokens['response-time'](req, res) + ' ms'
-        ].join('\t\t');
-
-        fs.appendFile("logs.json", logdata + "\n",  (err) => {
-            if (err)
-                console.log(err);
-        });
-    }
-
+server.use(morgan((tokens, req, res) => {
+	//const url = tokens.url(req, res);
+	// const validUrls = ["/api/v1/on-covid-19", "/api/v1/on-covid-19/json", "/api/v1/on-covid-19/xml"];
+	// if (validUrls.includes(url)) {//	};
+		const data = [
+		tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens['response-time'](req, res) +'  ms'
+  ].join('\t\t')
+		fs.appendFile("logs.json", data + "\n", (err) => {
+			if (err)
+				console.log(err);
+		});
 }));
 
 
 server.listen(config.PORT, () => {
-    mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+	mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
 
@@ -38,6 +35,6 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 
 db.once('open', () => {
-    require('./routes/covid19')(server);
-    console.log(`Server started on port ${config.PORT}`);
+	require('./routes/covid19')(server);
+	console.log(`Server started on port ${config.PORT}`);
 });
